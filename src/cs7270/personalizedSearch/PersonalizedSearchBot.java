c53abe7b5ef2fe5769cd31e57757992759a2697b
TKRKS <TKRKSN@gmail.com>
2015-02-14 00:20:27 -0500

    Can now read results and output to CSV file.

--- src/cs7270/personalizedSearch/PersonalizedSearchBot.java
+++ src/cs7270/personalizedSearch/PersonalizedSearchBot.java
@@ -7,81 +7,129 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.swing.*;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
-/* Right now this is just the demo from selenium's website.  We will alter it as we work on this project.  Its set to test using firefox becuase chrome involves additional weird setup stuff.*/
+import javax.swing.*;
 
 public class PersonalizedSearchBot {
 
-    public static void main(String[] args) {
+	private static class Result {
+		private String url;
+		private String title;
+		private String result;
+
+		public String getUrl() {
+			return url;
+		}
+	
+		public void setUrl(final String url) {
+			this.url = url;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+	
+		public void setTitle(final String title) {
+			this.title = title;
+		}
+
+		public String getResult() {
+			return result;
+		}
+	
+		public void setResult(final String result) {
+			this.result = result;
+		}
+	}
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+
+	final int pages = 5;
+	List<Result> results = new ArrayList<Result>();
 
         //Get the search, username and password
         final String searchTerm = JOptionPane.showInputDialog("Enter search term");
-        final String userName = JOptionPane.showInputDialog("Enter username");
+        String userName = JOptionPane.showInputDialog("Enter username");
         JPanel panel = new JPanel();
         JLabel label = new JLabel("Enter a password:");
         JPasswordField pass = new JPasswordField(20);
         panel.add(label);
         panel.add(pass);
         JOptionPane.showMessageDialog(null, panel);
-        final String password = new String(pass.getPassword());
+        String password = new String(pass.getPassword());
 
-        // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface, 
-        // not the implementation.
+        // Firefox driver since Chrome driver requires extra work
         final WebDriver driver = new FirefoxDriver();
 
-        // And now use this to visit Google
+	//Go to google
         driver.get("http://www.google.com");
+	//Login if necessary
         if ((!userName.isEmpty() && !password.isEmpty())) {
             driver.findElement(By.xpath("//a[text()='Sign in']")).click();
             driver.findElement(By.name("Email")).sendKeys(userName);
             driver.findElement(By.name("Passwd")).sendKeys(password);
             driver.findElement(By.name("signIn")).click();
-        }
+        } else {
+		userName = "Default";
+	}
 
-        // If using Google two factor, this will wait for the search page.
         new WebDriverWait(driver, 300).until(new ExpectedCondition<Boolean>() {
             @Override public Boolean apply(WebDriver webDriver) {
                 return driver.getTitle().toLowerCase().equals("google");
             }
         });
 
-        // Find the text input element by its name
+        //Enter the search
         WebElement element = driver.findElement(By.name("q"));
-
-        // Enter something to search for
         element.sendKeys(searchTerm);
-
-        // Now submit the form. WebDriver will find the form for us from the element
         element.submit();
 
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
-
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+        // Wait for Google to load
+        (new WebDriverWait(driver, 30)).until(new ExpectedCondition<Boolean>() {
             @Override public Boolean apply(WebDriver d) {
                 return d.getTitle().toLowerCase().startsWith(searchTerm);
             }
         });
 
 	//Read the first 5 pages
-	for (int i = 0; i < 5; i++) {
+	int rank = 1;
+	for (int i = 0; i < pages; i++) {
+		if (i != 0) {
+			//Wait for new page to laod
+			final int nextPageNum = i+ 1;
+			(new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+				public Boolean apply(WebDriver d) {
+		        		return d.findElement(By.id("resultStats")).getText().contains("Page " + (nextPageNum));
+				}
+			});
+		}
+		//Find all result host elements 
+		List<WebElement> resultElements = driver.findElements(By.xpath("//div[@class = 'srg']/li[@class = 'g']/div[@class = 'rc']/h3/a"));
+		for (WebElement resultElement : resultElements) {
+			Result result = new Result();			
+			result.setUrl(resultElement.getAttribute("href"));
+			result.setTitle(resultElement.getText());
+			result.setResult(Integer.toString(rank));
+			results.add(result);
+			rank++;
+		}
 		//Go to the next page
 		driver.findElement(By.id("pnnext")).click();
-		(new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-        		public Boolean apply(WebDriver d) {
-                		return d.getTitle().toLowerCase().startsWith(searchTerm);
-			}
-		});
 	}
 
-        // Should see: "cheese! - Google Search"
-        System.out.println("Page title is: " + driver.getTitle());
-
-        //Close the browser
+	//Write elements to CSV
+	String fileName = searchTerm + userName + ".csv";
+	PrintWriter output = new PrintWriter(searchTerm + userName + ".csv", "UTF-8");
+	output.println("Url;Title;Rank");
+	for (Result result : results) {
+		output.println(result.getUrl() + ";" + result.getTitle() + ";" + result.getResult());
+	}
+	output.close();
         driver.quit();
     }
 }
